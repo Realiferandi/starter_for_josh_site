b792c60c20edc56e0dd5f62fae67987e52552de5
Realiferandi <80743810+Realiferandi@users.noreply.github.com>
2021-03-17 20:04:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ wp_reset_query();?>
               </div>
               
                  <div class="col-md-4 article-cent">
-        <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh15/wp-content/themes/josh_theme_wp_fiinished/images/portrait.jpg">">
+        <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh15/wp-content/themes/starter_for_josh_site/images/image-1.jpg">
                   <h4> A PORTRAIT</h4>
                   <p class="date">12 December 2020</p>
                   <p>STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts. 
@@ -43,7 +43,7 @@ wp_reset_query();?>
           </div>
               
                    <div class="col-md-4 article-cent">
-            <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh15/wp-content/themes/josh_theme_wp_fiinished/images/model.jpg">>
+            <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh15/wp-content/themes/starter_for_josh_site/images/image-2.jpg">
                   <h4> A MODEL</h4>
                   <p class="date">12 December 2020</p>
                   <p>STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts. 
@@ -52,7 +52,7 @@ wp_reset_query();?>
           </div>
               
                    <div class="col-md-4 article-cent">
-            <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh15/wp-content/themes/starter_for_josh_site/images/game.jpg ">
+            <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh15/wp-content/themes/starter_for_josh_site/images/image-3.jpg ">
                   <h4> GAMES DESIGN</h4>
                   <p class="date">12 December 2020</p>
                   <p>STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts. 
