cba438150b816a69413fafc92cc18d593cb9a462
Realiferandi <80743810+Realiferandi@users.noreply.github.com>
2021-05-05 23:56:33 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -27,7 +27,7 @@
         <div class="row">
             <!--a row that gives us access to the BS columns-->
             <div>
-                <h3 class="text-center article-title "><?php the_field('titleforarticles'); ?></h3>
+                <h3 class="text-center article-title wow bounceInUp"><?php the_field('titleforarticles'); ?></h3>
             </div>
 
             <div class="col-md-4 article-cent">
